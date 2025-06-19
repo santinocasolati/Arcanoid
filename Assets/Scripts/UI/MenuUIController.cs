910b0ab22d06cead4790032e14ac57c12384a83d
santinocasolati <santicasolati@gmail.com>
2025-06-19 11:50:28 -0300

    Music Added

--- Assets/Scripts/UI/MenuUIController.cs
+++ Assets/Scripts/UI/MenuUIController.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class MenuUIController : MonoBehaviour
 {
     [SerializeField] private int gameSceneIndex;
+    [SerializeField] private AudioMixer audioMixer;
 
     public void Play()
     {
         SceneManager.LoadSceneAsync(gameSceneIndex, LoadSceneMode.Single);
+    }
+
+    public void SetAudioVolume(float value)
+    {
+        float db = Mathf.Log10(Mathf.Clamp(value, 0.0001f, 1f)) * 20f;
+        audioMixer.SetFloat("Volume", db);
     }
 
     public void Exit()
